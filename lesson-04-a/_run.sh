e667f699371474657d83c9e387e0fb6a7dd82868
Sergei <sleepytomcat@gmail.com>
2018-03-23 16:48:00 -0700

    Gradle basics, java plugin

---
+++ lesson-04-a/_run.sh
@@ -0,0 +1,6 @@
+# list available Gradle tasks
+gradle tasks
+# same list, extra details
+gradle tasks --all
+# execute 'hello' task
+gradle hello
---
+++ lesson-04-b/_run.sh
@@ -0,0 +1,6 @@
+# list available Gradle tasks
+gradle tasks
+# same list, extra details (note new tasks introduced by 'java' plugin)
+gradle tasks --all
+# execute 'hello' task
+gradle hello
