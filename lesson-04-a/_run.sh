2f47a8c1338071fdce1d018cfbb2f6f4728c856d
Sergei <sleepytomcat@gmail.com>
2018-03-23 16:37:50 -0700

    Revert "Gradle basics"
    This reverts commit 98497315065707f7d04b3c090bf153d96c7af1f8.

--- lesson-04-a/_run.sh
+++
@@ -1,4 +0,0 @@
-# list all available Gradle tasks for this project:
-gradle tasks
-# execute our custom 'hello' task:
-gradle hello
--- lesson-04-b/_run.sh
+++
@@ -1,5 +0,0 @@
-# list all available Gradle tasks for this project:
-# (note there're extra tasks added by 'java' plugin)
-gradle tasks
-# execute our custom 'hello' task:
-gradle hello
