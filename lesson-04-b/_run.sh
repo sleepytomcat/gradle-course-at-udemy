e73bbb6b664c1ca61262be5f53124ba95013d779
Sergei <sleepytomcat@gmail.com>
2018-03-23 17:10:02 -0700

    Building some basic Java project

--- lesson-04-b/_run.sh
+++ lesson-04-b/_run.sh
@@ -1,6 +1,13 @@
-# list available Gradle tasks
-gradle tasks
-# same list, extra details (note new tasks introduced by 'java' plugin)
+# list of available Gradle (note new tasks introduced by 'java' plugin)
 gradle tasks --all
-# execute 'hello' task
-gradle hello
+
+# clean all build output
+gradle clean
+
+# 'build' task builds the Java project, creating ./build output directory;
+# Gradle 'java' plugin expects particular file structure:
+# src/main/java - production Java sources
+# src/main/resources - production resources
+# src/test/java - test Java sources
+# src/test/resources - test resources
+gradle build
