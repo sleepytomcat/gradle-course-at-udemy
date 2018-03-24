8641a3671db7d62a4f3f5abd14789a3738a28299
Sergei <sleepytomcat@gmail.com>
2018-03-23 17:58:27 -0700

    Defining descriptions and action(s) for tasks

---
+++ lesson-06/_run.sh
@@ -0,0 +1,13 @@
+# list of available Gradle (note new tasks introduced by 'java' plugin)
+# note assorted custom tasks (with associated descriptions) created for this project
+gradle tasks --all
+
+# let's run one of the tasks (nothing happens, as there're no actions defined so far
+gradle Task1
+
+# let's run some tasks
+gradle Task2
+gradle Task3
+gradle Task4
+gradle Task5
+gradle Task6
